77ca63924bb29b6d29f0b8102259db287120c953
Mirjalol Mirxomitov <miramirxomitov97@gmail.com>
2020-11-27 14:46:32 +0500

    change site name

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: `Журналистларни қайта таерлаш маркази`,
+    title: `Журналистларни қайта тайёрлаш маркази`,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
