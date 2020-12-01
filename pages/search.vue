d822025691b0dfa78816f60ffc835da06ba0bec6
Mirjalol Mirxomitov <miramirxomitov97@gmail.com>
2020-12-01 10:00:53 +0500

    added treners

--- pages/search.vue
+++ pages/search.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="main-search">
     <b-container>
-      <h1>Search</h1>
+      <h1>{{$t('search.title')}}</h1>
       <div class="search__list" v-for="(search, index) in getInput" :key="index.id">
         <nuxt-link :to="localePath(`/posts/${search.id}`)" class="search__title">{{search.title}}</nuxt-link>
+        
       </div>
     </b-container>
   </div>
@@ -31,5 +32,12 @@ export default {
 <style>
   .main-search {
     padding: 50px 0;
+  }
+  .main-search h1 {
+    margin-bottom: 15px;
+  }
+  .search__title {
+    margin-bottom: 10px;
+    display: block;
   }
 </style>
